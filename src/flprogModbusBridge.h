528a2941cffe9cfa9c433b8b3d4d3e4c83fe8c70
Totuin <sglushenco@yandex.ru>
2022-11-05 04:01:15 +0500

    Initial commit

---
+++ src/flprogModbusBridge.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "Arduino.h"
+
+
+class ModbusBridge 
+{
+public:
+protected:
+private:
+};
+
+class ModbusBridgeTCPDevice
+{
+public:
+protected:
+private:
+};
+
+class ModbusBridgeRTUDevice
+{
+public:
+protected:
+
+private:
+};
