4a62888dffcb37865ebf22772f70adf91e4f339c
Totuin <sglushenco@yandex.ru>
2022-11-05 05:11:36 +0500

    AddBridgeNext

--- src/flprogModbusBridge.cpp
+++ src/flprogModbusBridge.cpp
@@ -1,1 +1,96 @@
 #include "flprogModbusBridge.h"
+
+void ModbusBridge::pool()
+{
+    TcpPool();
+    RtuPool();
+}
+
+void ModbusBridge::tcpPool()
+{
+    tcpDevice->connect();
+
+    if (!tcpDevice->connected())
+    {
+        return;
+    }
+    byte readingByte;
+    byte byteIndex = 0;
+    if (tcpDevice->available())
+    {
+        bufferSize = 0;
+        while (tcpDevice->available())
+        {
+            readingByte = tcpDevice->read();
+            if (byteIndex < 6)
+            {
+                mbapBuffer[byteIndex] = readingByte;
+            }
+            else
+            {
+                buffer[bufferSize] = readingByte;
+                bufferSize++;
+            }
+            byteIndex++;
+        }
+        sendRTUBuffer();
+    }
+}
+
+void ModbusBridge::rtuPool()
+{
+    byte avalibleBytes = rtuDevice->available();
+    if (avalibleBytes == 0)
+        return;
+    if (avalibleBytes != lastRec)
+    {
+        lastRec = avalibleBytes;
+        startT35 = millis();
+        return;
+    }
+    if (!(flprog::isTimer(startT35, (rtuDevice->t35TimeForSpeed()))))
+        return;
+    lastRec = 0;
+    getRTURxBuffer();
+    if (bufferSize < 5)
+    {
+        return;
+    }
+    sendTCPBuffer();
+}
+
+void ModbusBridge::getRTURxBuffer()
+{
+    bufferSize = 0;
+    while (rtuDevice->available())
+    {
+        buffer[bufferSize] = rtuDevice->read();
+        bufferSize++;
+    }
+}
+
+void ModbusBridge::sendTCPBuffer()
+{
+    if (!tcpDevice->connected())
+    {
+        return;
+    }
+    bufferSize -= 2;
+    mbapBuffer[4] = highByte(bufferSize);
+    mbapBuffer[5] = lowByte(bufferSize);
+    tcpDevice->write(mbapBuffer, 6);
+    tcpDevice->write(buffer, bufferSize);
+    bufferSize = 0;
+}
+
+void ModbusBridge::sendRTUBuffer()
+{
+    byte i = 0;
+    int crc = flprogModus::modbusCalcCRC(bufferSize, buffer);
+    buffer[bufferSize] = crc >> 8;
+    bufferSize++;
+    buffer[bufferSize] = crc & 0x00ff;
+    bufferSize++;
+    rtuDevice->write(buffer, bufferSize);
+    bufferSize = 0;
+}
--- src/flprogModbusBridge.h
+++ src/flprogModbusBridge.h
@@ -1,17 +1,38 @@
 #pragma once
 #include "Arduino.h"
+#include "flprogModbusUtilites.h"
 
-
-class ModbusBridge 
+class ModbusBridge
 {
 public:
+    void pool();
+
 protected:
 private:
+    void tcpPool();
+    void rtuPool();
+    void sendRTUBuffer();
+    void getRTURxBuffer();
+    void sendTCPBuffer();
+    byte bufferSize = 0;
+    byte mbapBuffer[6];
+    byte buffer[64];
+    byte lastRec = 0;
+    unsigned long startT35;
+    ModbusBridgeTCPDevice *tcpDevice;
+    ModbusBridgeRTUDevice *rtuDevice;
 };
 
 class ModbusBridgeTCPDevice
 {
 public:
+    virtual byte available(){};
+    virtual byte read(){};
+    virtual bool connected(){};
+    virtual void stop(){};
+    virtual void connect(){};
+    virtual byte write(byte buffer[], byte size){};
+
 protected:
 private:
 };
@@ -19,7 +40,11 @@ private:
 class ModbusBridgeRTUDevice
 {
 public:
-protected:
+    virtual byte available(){};
+    virtual byte read(){};
+    virtual unsigned long t35TimeForSpeed(){};
+    virtual byte write(byte buffer[], byte size){};
 
+protected:
 private:
 };
